dd170d2f863e4f69f1fcafbfdae06e1cb25607d0
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-06-08 20:00:00 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
-5.times { Category.create(name: Faker::Emotion.noun) }
-
-5.times { User.create(name: Faker::Movies::BackToTheFuture.character, email: Faker::Internet.email) }
+Category.create([{ name: 'Black' }, { name: 'White' }, { name: 'Red' }])
+User.create([{ name: 'Bin', email: 'bin@bin.ru' }, { name: 'Min', email: 'min@bin.ru' }])
 
 categories = Category.all
 users = User.all
 states = Bulletin.aasm.states.map(&:name)
 
-200.times do
+200.times do |i|
   bulletin = Bulletin.new(
-    title: Faker::Beer.name,
-    description: Faker::Lorem.paragraph_by_chars,
+    title: "bul #{i}",
+    description: "Description #{i}",
     user: users.sample,
     category: categories.sample,
     state: states.sample
