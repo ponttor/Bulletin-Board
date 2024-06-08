10854d3767b63d7131a1274f72392e5cd4b138e8
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-06-08 12:24:57 +0200

    ref #1: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
-5.times do
-  Category.create(name: Faker::Emotion.noun)
-end
+5.times {Category.create(name: Faker::Emotion.noun)}
 
-3.times do
-  User.create(name: Faker::Movies::BackToTheFuture.character, email: Faker::Internet.email)
-end
+5.times {User.create(name: Faker::Movies::BackToTheFuture.character, email: Faker::Internet.email)}
 
-b1 = Bulletin.create(user: User.first, category: Category.first, description: Faker::Movies::BackToTheFuture.quote, title: Faker::Ancient.hero)
-b1.image.attach(io: Rails.root.join('db/images/bul1.jpeg').open, filename: 'bul1.jpeg')
-b2 = Bulletin.create(user: User.first, category: Category.first, description: Faker::Movies::BackToTheFuture.quote, title: Faker::Ancient.hero)
-b2.image.attach(io: Rails.root.join('db/images/bul2.jpeg').open, filename: 'bul2.jpeg')
-b3 = Bulletin.create(user: User.first, category: Category.first, description: Faker::Movies::BackToTheFuture.quote, title: Faker::Ancient.hero)
-b3.image.attach(io: Rails.root.join('db/images/bul3.jpeg').open, filename: 'bul3.jpeg')
-b4 = Bulletin.create(user: User.first, category: Category.first, description: Faker::Movies::BackToTheFuture.quote, title: Faker::Ancient.hero)
-b4.image.attach(io: Rails.root.join('db/images/bul4.jpeg').open, filename: 'bul4.jpeg')
+categories = Category.all
+users = User.all
+states = Bulletin.aasm.states.map(&:name)
+
+200.times do
+  bulletin = Bulletin.new(
+    title: Faker::Beer.name,
+    description: Faker::Lorem.paragraph_by_chars,
+    user: users.sample,
+    category: categories.sample,
+    state: states.sample
+  )
+
+  bulletin.image.attach(io: Rails.root.join('db/images/bul1.jpeg').open, filename: 'bul1.jpeg')
+
+  bulletin.save!
+
+end
