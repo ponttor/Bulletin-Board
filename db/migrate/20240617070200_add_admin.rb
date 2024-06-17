5da4eec9fa5ddd3aeb33362679b03b4bc8b4540b
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-06-17 09:06:59 +0200

    add admin

---
+++ db/migrate/20240617070200_add_admin.rb
@@ -0,0 +1,6 @@
+class AddAdmin < ActiveRecord::Migration[7.1]
+  def change
+    user = User.find_by(email: 'vasiliqa13@gmail.com')
+    user.update(admin: true)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-# Category.create([{ name: 'Black' }, { name: 'White' }, { name: 'Red' }])
-# User.create([{ name: 'Bin', email: 'bin@bin.ru' }, { name: 'Min', email: 'min@bin.ru' }])
+Category.create([{ name: 'Black' }, { name: 'White' }, { name: 'Red' }])
+User.create([{ name: 'Bin', email: 'bin@bin.ru' }, { name: 'Min', email: 'min@bin.ru' }])
 
-# categories = Category.all
-# users = User.all
-# states = Bulletin.aasm.states.map(&:name)
+categories = Category.all
+users = User.all
+states = Bulletin.aasm.states.map(&:name)
 
-# 100.times do |i|
-#   bulletin = Bulletin.new(
-#     title: "bul #{i}",
-#     description: "Description #{i}",
-#     user: users.sample,
-#     category: categories.sample,
-#     state: states.sample
-#   )
+100.times do |i|
+  bulletin = Bulletin.new(
+    title: "bul #{i}",
+    description: "Description #{i}",
+    user: users.sample,
+    category: categories.sample,
+    state: states.sample
+  )
 
-#   bulletin.image.attach(io: Rails.root.join('db/images/bul1.jpeg').open, filename: 'bul1.jpeg')
+  bulletin.image.attach(io: Rails.root.join('db/images/bul1.jpeg').open, filename: 'bul1.jpeg')
 
-#   bulletin.save!
-#   sleep 0.5
-# end
+  bulletin.save!
+  sleep 0.5
+end
