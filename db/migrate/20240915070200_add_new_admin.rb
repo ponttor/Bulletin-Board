5807cb9bfee6ac8731cf60a158c295eb7a975d9e
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-09-15 13:04:20 +0200

    add admin

---
+++ db/migrate/20240915070200_add_new_admin.rb
@@ -0,0 +1,8 @@
+class AddAdmin < ActiveRecord::Migration[7.1]
+  def change
+    return unless Rails.env.production?
+
+    user = User.find_by(email: 'ruzinovrun@gmail.com')
+    user.update(admin: true) if user
+  end
+end
